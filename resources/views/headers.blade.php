5d1b34a8819b0891e10bbcfba2231260e453f474
Malander <marimichele26@gmail.com>
2015-11-13 17:24:23 +0100

    234
    234

--- resources/views/headers.blade.php
+++ resources/views/headers.blade.php
@@ -3,6 +3,6 @@
 <meta property="og:url" content="http://stillpoint.sidrit.com/articolo/{{$item->slug}}" />
 <meta property="og:description" content="{{$item->excerpt}}" />
 <meta property="fb:app_id" content="198258987176383" />
-<meta property="og:image" content="http://stillpoint.sidrit.com/uploads/{{$item->featuredImage->id}}/{{$item->featuredImage->file_name}}">
+<meta property="og:image" content="http://stillpoint.sidrit.com/uploads/{{$item->featuredImage->id}}/{{urlencode($item->featuredImage->file_name)}}">
 <meta property="og:type" content="article" />
 <meta property="og:locale:alternate" content="it_IT" />  <!-- Italian -->
