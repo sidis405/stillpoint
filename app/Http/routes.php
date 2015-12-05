761d50f54c16573eb4b4db22824d21f55333ec70
Sidrit Trandafili <forge405@gmail.com>
2015-12-05 13:00:08 +0100

    changes sid - stillpoint.dev Sat Dec  5 13:00:06 CET 2015

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -13,8 +13,8 @@ Route::get('auth/logout', 'Auth\AuthController@getLogout');
 
 
 // // Registration routes...
-Route::get('auth/register', 'Auth\AuthController@getRegister');
-Route::post('auth/register', 'Auth\AuthController@postRegister');
+// Route::get('auth/register', 'Auth\AuthController@getRegister');
+// Route::post('auth/register', 'Auth\AuthController@postRegister');
 
 Route::post('contact_send', '\Stillpoint\Http\Controllers\MailController@send');
 
